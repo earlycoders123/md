1b5b2b3be4652c88857a2d1768e6ccbf557ba978
earlycoders123 <solanki.nidhi@gmail.com>
2025-07-18 19:35:03 +0530

    Update colorSplash.py

--- colorSplash.py
+++ colorSplash.py
@@ -9,9 +9,9 @@ genai.configure(api_key="AIzaSyDI5Hr2zxpxm3ZyfCGgO5iTWeAp_eprUaA")  # Paste actu
 model = genai.GenerativeModel('gemini-2.5-pro')
 
 # Generate Story
-prompt = "Write a short, colorful story for kids about a magical cat."
+user_prompt = input("Enter your story prompt: ")
 
-response = model.generate_content(prompt)
+response = model.generate_content(user_prompt)
 
 # Show the story
 print(response.text)
