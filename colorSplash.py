8c000e0e8c3efdf531590ba430a88d57a5af8fa1
earlycoders123 <solanki.nidhi@gmail.com>
2025-07-18 19:49:09 +0530

    Update colorSplash.py

--- colorSplash.py
+++ colorSplash.py
@@ -7,7 +7,7 @@ import streamlit as st
 genai.configure(api_key="AIzaSyDI5Hr2zxpxm3ZyfCGgO5iTWeAp_eprUaA")  # Paste actual key
 
 # Use Gemini Pro Model
-model = genai.GenerativeModel('gemini-2.5-pro')
+model = genai.GenerativeModel('gemini-pro')
 
 # Generate Story
 user_prompt = st.text_input("Enter your story prompt:")
